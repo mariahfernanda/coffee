35c6442e1553009b9857a7b36be27abaaca78d92
MARIA FERNANDA BENTO DA SILVA <21276378@CFP127ALUNO.LOCAL>
2022-03-22 16:58:16 -0300

    22/03

---
+++ cms/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div id="login-container">
+        <h1>Autenticação para o CMS</h1>
+        <h2>InOut Coffee</h2>
+        <form action="">
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email" placeholder="Digite seu email" autocomplete="off">
+             <label for="password">Senha</label>
+             <input type="password" name="password" id="password" placeholder="Digite sua senha">
+             <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
